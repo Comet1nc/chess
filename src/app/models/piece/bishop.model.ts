8ec26834503fe09045c8c245abc2f236580f9135
Ivan <zrosji@gmail.com>
2023-08-26 19:00:02 +0200

    highlighting cell

--- src/app/models/piece/bishop.model.ts
+++ src/app/models/piece/bishop.model.ts
@@ -3,10 +3,34 @@ import { Color } from '../color.model';
 import { CoordinatesShift } from '../coordinates-shift.model';
 import { Coordinates } from '../coordinates.model';
 import { Piece } from './piece.model';
+import { Board } from '../board.model';
 
 export class Bishop extends Piece {
   sprite = this.color === Color.WHITE ? '&#x2657;' : '&#x265D;';
   override getPieceMoves(): CoordinatesShift[] {
-    throw new Error('Method not implemented.');
+    let result: CoordinatesShift[] = [];
+    // bottom-left to top-right
+    for (let i = -7; i <= 7; i++) {
+      if (i === 0) continue;
+      result.push(new CoordinatesShift(i, i));
+    }
+
+    // top-left to bottom-right
+    for (let i = -7; i <= 7; i++) {
+      if (i === 0) continue;
+      result.push(new CoordinatesShift(i, -i));
+    }
+
+    return result;
+  }
+
+  override isSquareAvailableForMove(
+    coordinates: Coordinates,
+    board: Board
+  ): boolean {
+    return (
+      board.isSquareEmpty(coordinates) ||
+      board.getPiece(coordinates)?.color !== this.color
+    );
   }
 }
